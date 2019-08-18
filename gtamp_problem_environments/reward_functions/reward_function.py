db35ce76b0ab6adff42ae81cb0196a1881ee69fc
Beomjoon Kim <beomjoon@mit.edu>
2019-08-18 17:36:14 -0400

    new reward function based on planning horizon

--- gtamp_problem_environments/reward_functions/reward_function.py
+++ gtamp_problem_environments/reward_functions/reward_function.py
@@ -1,13 +1,15 @@
 class AbstractRewardFunction:
-    def __init__(self, problem_env, goal_objects, goal_region):
+    def __init__(self, problem_env, goal_objects, goal_region, planning_horizon):
         self.problem_env = problem_env
         self.robot = problem_env.robot
         self.goal_objects = goal_objects
         self.goal_region = goal_region
-        self.infeasible_reward = -2
+        self.infeasible_reward = -9999
+        self.goal_reward = planning_horizon
         self.achieved = []
+        self.planning_horizon = planning_horizon
 
-    def __call__(self, curr_state, next_state, action):
+    def __call__(self, curr_state, next_state, action, time_step):
         raise NotImplementedError
 
     def is_goal_reached(self):
@@ -15,15 +17,19 @@ class AbstractRewardFunction:
 
 
 class GenericRewardFunction(AbstractRewardFunction):
-    def __init__(self, problem_env, goal_objects, goal_region):
-        AbstractRewardFunction.__init__(self, problem_env, goal_objects, goal_region)
+    def __init__(self, problem_env, goal_objects, goal_region, planning_horizon):
+        AbstractRewardFunction.__init__(self, problem_env, goal_objects, goal_region, planning_horizon)
 
-    def __call__(self, curr_state, next_state, action):
+    def __call__(self, curr_state, next_state, action, time_step):
         if action.is_skeleton:
             return 0
         else:
+            is_infeasible_action = next_state is None
+            remaining_steps = self.planning_horizon - time_step
             if self.is_goal_reached():
-                return 1
+                return self.goal_reward
+            elif is_infeasible_action:
+                return -1*remaining_steps
             else:
                 return -1
 
