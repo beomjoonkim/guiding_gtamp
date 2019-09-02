87d75d440f74e2b56e345a477332a26d0a4d1a5c
beomjoon <beomjoon@mit.edu>
2019-09-02 13:09:28 -0400

    adding options

--- test_scripts/multithreaded/threaded_test_utils.py
+++ test_scripts/multithreaded/threaded_test_utils.py
@@ -5,17 +5,19 @@ def parse_options():
     parser = argparse.ArgumentParser(description='Greedy Planner parameters')
     parser.add_argument('-n_objs_pack', type=int, default=1)
     parser.add_argument('-planner_seed', type=int, default=0)
-    parser.add_argument('-timelimit', type=int, default=1000)
+    parser.add_argument('-timelimit', type=int, default=300)
     parser.add_argument('-loss', type=str, default='largemargin')
     parser.add_argument('-pidxs', nargs=2, type=int, default=[0, 1])
     parser.add_argument('-num_train', type=int, default=5000)
     parser.add_argument('-domain', type=str, default='two_arm_mover')
     parser.add_argument('-train_seed', nargs=2, type=int, default=[0, 1])
     parser.add_argument('-use_shaped_reward', action='store_true', default=False)
+    parser.add_argument('-use_q_count', action='store_true', default=False)
     parser.add_argument('-sampling_strategy', type=str, default='uniform')
     parser.add_argument('-hcount', action='store_true', default=False)
     parser.add_argument('-state_hcount', action='store_true', default=False)
     parser.add_argument('-explr_p', type=float, default=0.3)  # number of re-evals
+    parser.add_argument('-ucb_parameter', type=float, default=1)  # number of re-evals
 
     parameters = parser.parse_args()
     return parameters
@@ -66,6 +68,9 @@ def get_mcts_configs():
         config['explr_p'] = parameters.explr_p
         if parameters.use_shaped_reward:
             config['use_shaped_reward'] = ""
+        if parameters.use_q_count:
+            config['use_q_count'] = ""
+        config['ucb_parameter'] = parameters.ucb_parameter
 
     return configs
 
