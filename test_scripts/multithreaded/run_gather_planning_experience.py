b59902e162c386e24fa34748950a7669c5d955dd
beomjoon <beomjoon@mit.edu>
2019-09-14 13:34:49 -0400

    gather plan experience

--- test_scripts/multithreaded/run_gather_planning_experience.py
+++ test_scripts/multithreaded/run_gather_planning_experience.py
@@ -1,18 +1,19 @@
 import os
 import multiprocessing
+import argparse
 
 from multiprocessing.pool import ThreadPool  # dummy is nothing but multiprocessing but wrapper around threading
 from threaded_test_utils import get_sahs_configs
 
 
 def worker_p(config):
-    command = 'python ./test_scripts/gather_planning_experience.py'
+    command = 'python ./test_scripts/run_greedy.py'
 
     for key, value in zip(config.keys(), config.values()):
-        if 'sampling_strategy' in key:
-            continue
         option = ' -' + str(key) + ' ' + str(value)
         command += option
+    command += ' -hcount'
+    command += ' -gather_planning_exp'
 
     print command
     os.system(command)
@@ -23,7 +24,21 @@ def worker_wrapper_multi_input(multi_args):
 
 
 def main():
-    configs = get_sahs_configs()
+    parser = argparse.ArgumentParser(description='Greedy Planner parameters')
+    parser.add_argument('-pidxs', nargs=2, type=int, default=[0, 1])
+    parser.add_argument('-domain', type=str, default='two_arm_mover')
+    parameters = parser.parse_args()
+    pidx_begin = parameters.pidxs[0]
+    pidx_end = parameters.pidxs[1]
+    configs = []
+    for pidx in range(pidx_begin, pidx_end):
+        config = {
+            'pidx': pidx,
+            'domain': parameters.domain,
+        }
+
+        configs.append(config)
+
     n_workers = multiprocessing.cpu_count()
     pool = ThreadPool(n_workers)
     results = pool.map(worker_wrapper_multi_input, configs)
