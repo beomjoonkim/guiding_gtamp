3d296e0c68f291fc9f8d5c5c91ca40fbccc20629
beomjoon <beomjoon@mit.edu>
2019-09-15 19:06:32 -0400

    going back to 1

--- test_scripts/multithreaded/run_sahs.py
+++ test_scripts/multithreaded/run_sahs.py
@@ -51,7 +51,7 @@ def main():
 
         configs.append(config)
 
-    n_workers = multiprocessing.cpu_count()
+    n_workers = 1 #multiprocessing.cpu_count()
     pool = ThreadPool(n_workers)
     results = pool.map(worker_wrapper_multi_input, configs)
 
