daaa39e2f6467c62921f696d36e488692e0a2b7f
beomjoon <beomjoon@mit.edu>
2019-09-18 15:11:31 -0400

    process dirs

---
+++ data_processing/sampler/threaded_process_planning_experience.py
@@ -0,0 +1,44 @@
+import os
+import sys
+from multiprocessing.pool import ThreadPool  # dummy is nothing but multiprocessing but wrapper around threading
+import multiprocessing
+import argparse
+import time
+
+from process_planning_experience import parse_parameters
+
+
+def worker_p(config):
+    command = 'python ./data_processing/sampler/process_planning_experience.py'
+
+    for key, value in zip(config.keys(), config.values()):
+        option = ' -' + str(key) + ' ' + str(value)
+        command += option
+
+    print command
+    os.system(command)
+
+
+def worker_wrapper_multi_input(multi_args):
+    time.sleep(1)
+    return worker_p(multi_args)
+
+
+def main():
+    params = parse_parameters()
+    param_vals = vars(params)
+    configs = []
+
+    for pidx in range(5000):
+        config = {key: value for key, value in zip(param_vals.keys(), param_vals.values())}
+        config['pidx'] = pidx
+        configs.append(config)
+
+    n_workers = multiprocessing.cpu_count()
+    print configs
+    pool = ThreadPool(n_workers)
+    results = pool.map(worker_wrapper_multi_input, configs)
+
+
+if __name__ == '__main__':
+    main()
