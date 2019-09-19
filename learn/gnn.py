7c35c937d340d50ccf845aa71973a7991e231ba8
beomjoon <beomjoon@mit.edu>
2019-09-19 14:12:59 -0400

    adding old weights

--- learn/gnn.py
+++ learn/gnn.py
@@ -30,6 +30,9 @@ class GNN(object):
             print self.entity_name_to_idx
 
     def load_weights(self):
+        self.weight_file_name = './learn/q-function-weights/Q_weight_n_msg_passing_1_mse_weight_1.0_optimizer_' \
+                                'adam_seed_%d_lr_0.0001_operator_two_arm_pick_two_arm_place_n_layers_2_n_hidden_32' \
+                                '_top_k_1_num_train_5000_loss_largemargin.hdf5' % self.config.seed
         print "Loading weight", self.weight_file_name
         self.loss_model.load_weights(self.weight_file_name)
 
