f23b59e6c25b74bba766fcec4fd64a8831b29d42
beomjoon <beomjoon@mit.edu>
2019-09-20 10:32:33 -0400

    adding proper names

--- test_scripts/run_greedy.py
+++ test_scripts/run_greedy.py
@@ -61,6 +61,19 @@ def get_solution_file_name(config):
                              + '/mse_weight_' + str(config.mse_weight) + '/use_region_agnostic_' + str(
             config.use_region_agnostic) \
                              + '/mix_rate_' + str(config.mixrate) + '/'
+    elif config.qlearned_hcount_new_number_in_goal:
+        solution_file_dir += '/qlearned_hcount_obj_already_in_goal_new_number_in_goal/shortest_irsc/loss_' + str(
+            config.loss) + '/num_train_' + str(config.num_train) \
+                             + '/mse_weight_' + str(config.mse_weight) + '/use_region_agnostic_' + str(
+            config.use_region_agnostic) \
+                             + '/mix_rate_' + str(config.mixrate) + '/'
+    elif config.qlearned_hcount_old_number_in_goal:
+        solution_file_dir += '/qlearned_hcount_obj_already_in_goal_old_number_in_goal/shortest_irsc/loss_' + str(
+            config.loss) + '/num_train_' + str(config.num_train) \
+                             + '/mse_weight_' + str(config.mse_weight) + '/use_region_agnostic_' + str(
+            config.use_region_agnostic) \
+                             + '/mix_rate_' + str(config.mixrate) + '/'
+
     elif config.qlearned_old_number_in_goal:
         solution_file_dir += '/qlearned_old_number_in_goal/shortest_irsc/loss_' + str(
             config.loss) + '/num_train_' + str(config.num_train) \
@@ -70,11 +83,13 @@ def get_solution_file_name(config):
     elif config.qlearned_new_number_in_goal:
         solution_file_dir += '/qlearned_new_number_in_goal/shortest_irsc/loss_' + str(
             config.loss) + '/num_train_' + str(config.num_train) \
-                             + '/mse_weight_' + str(config.mse_weight)
-    else:
+                             + '/mse_weight_' + str(config.mse_weight)+"/"
+    elif config.pure_learned_q:
         solution_file_dir += '/gnn/shortest_irsc/loss_' + str(config.loss) + '/num_train_' + str(config.num_train) \
                              + '/mse_weight_' + str(config.mse_weight) + '/use_region_agnostic_' \
                              + str(config.use_region_agnostic) + '/'
+    else:
+        raise NotImplementedError
     solution_file_name = 'pidx_' + str(config.pidx) + \
                          '_planner_seed_' + str(config.planner_seed) + \
                          '_train_seed_' + str(config.train_seed) + \
@@ -229,7 +244,6 @@ def main():
             'num_nodes': num_nodes,
             'plan': plan
         }
-
         with open(solution_file_name, 'wb') as f:
             pickle.dump(data, f)
     print 'Time: %.2f Success: %d Plan length: %d Num nodes: %d' % (tottime, success, plan_length, num_nodes)
