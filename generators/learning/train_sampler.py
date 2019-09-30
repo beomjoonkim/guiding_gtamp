cce3c69dd974a8690e3a463f34479646a8df0562
Beomjoon Kim <beomjoon@phaedra.csail.mit.edu>
2019-09-30 07:59:52 -0400

    removing weights

--- generators/learning/train_sampler.py
+++ generators/learning/train_sampler.py
@@ -139,7 +139,7 @@ def train_admon_with_pose(config):
     savedir = './generators/learning/learned_weights/'
     admon = AdversarialMonteCarloWithPose(dim_action=dim_action, dim_collision=dim_state,
                                           save_folder=savedir, tau=config.tau, config=config)
-    admon.train(states, poses, actions, sum_rewards, epochs=100)
+    admon.train(states, poses, actions, sum_rewards, epochs=500)
 
 
 def parse_args():
