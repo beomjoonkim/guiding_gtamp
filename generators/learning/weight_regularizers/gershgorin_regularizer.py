5ce570a86bd010b74c38eb3fa992c6a221a3936f
beomjoon <beomjoon@mit.edu>
2019-10-14 15:23:39 -0400

    increased g regularization weight

--- generators/learning/weight_regularizers/gershgorin_regularizer.py
+++ generators/learning/weight_regularizers/gershgorin_regularizer.py
@@ -1,9 +1,10 @@
+
+
 import tensorflow as tf
-import numpy as np
 
 
-# sess = tf.Session()
-def gershgorin_reg(weight_matrix, sess=None):
+#
+def gershgorin_reg(weight_matrix):
     weight_matrix = tf.squeeze(weight_matrix)
     wdotw = tf.matmul(tf.transpose(weight_matrix), weight_matrix)
 
@@ -19,24 +20,14 @@ def gershgorin_reg(weight_matrix, sess=None):
 
     # regularize it if the lower bound is less than 1, but otherwise don't penalize
     delta = 1.0 - gershgorin_lb
-    hinge_loss_on_gershgorin_lb = tf.maximum(tf.cast(0., tf.float32), delta)
+    hinge_loss_on_gershgorin_lb = 5*tf.maximum(tf.cast(0., tf.float32), delta)
 
+    """
     if sess is not None:
         print diag_vals.eval(session=sess)
         print abs_sum_offdiag_vals.eval(session=sess)
         print 'G lower bound', gershgorin_lb.eval(session=sess)
         print "Hinge loss", hinge_loss_on_gershgorin_lb.eval(session=sess)
+    """
 
     return hinge_loss_on_gershgorin_lb
-
-
-def test_correctness():
-    # todo train the weights, load them, and evaluate its Gershgorin value
-    test_mat = np.array([[1, -1, -1, -1], [4, 1, 2, 3]])
-    print np.dot(test_mat.transpose(), test_mat)
-    test_mat = tf.constant(test_mat, dtype=tf.float32)
-    loss = gershigorin_reg(test_mat)
-
-
-if __name__ == '__main__':
-    test_correctness()
---
+++ generators/learning/weight_regularizers/test_trained_weight_g_vals.py
@@ -0,0 +1,32 @@
+from generators.learning.utils.model_creation_utils import create_imle_model
+from generators.learning.weight_regularizers.gershgorin_regularizer import gershgorin_reg
+
+import numpy as np
+import tensorflow as tf
+
+
+def test_correctness():
+    seed = 0
+    #test_mat = np.array([[1, -1, -1, -1], [4, 1, 2, 3]])
+    policy = create_imle_model(seed)
+    layers = policy.policy_model.layers
+
+    gershgorin_vals = 1
+    sess = tf.Session()
+    for layer in layers:
+        if len(layer.get_weights()) == 0:
+            continue
+        weight = layer.get_weights()[0].squeeze()
+        if len(weight.shape) <= 1:
+            continue
+        test_mat = tf.constant(weight, dtype=tf.float32)
+        loss = gershgorin_reg(test_mat).eval(session=sess)
+        if loss == 0:
+            import pdb;pdb.set_trace()
+        gershgorin_vals *= loss
+        print gershgorin_vals
+    import pdb;pdb.set_trace()
+
+
+if __name__ == '__main__':
+    test_correctness()
